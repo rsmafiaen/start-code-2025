53f4861800e37361a1613c561b9c8c59d39d2afa
Nicolay B. Rennemo <rennemo@proton.me>
2025-10-17 23:15:07 +0200

    Lint

--- whitewash/src/app/components/headerItem.tsx
+++ whitewash/src/app/components/headerItem.tsx
@@ -1,8 +1,9 @@
+import type { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import Link from "next/link";
 
 type headerItemProps = {
-    img: any, 
+    img: string | StaticImport, 
     link: string,
     title: string
 }
@@ -10,7 +11,7 @@ type headerItemProps = {
 export function HeaderItem(props: headerItemProps) {
     return (
         <div className="mr-8">
-            <Link className="flex flex-row" href={"/" + props.link}>
+            <Link className="flex flex-row" href={`/${props.link}`}>
                 <Image src={props.img} alt={""} />
                 <span>{props.title}</span>
             </Link>
--- whitewash/src/app/page.tsx
+++ whitewash/src/app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import frawg from "../../frog.png";
+
 import { Header } from "./components/header";
 import { Oddy } from "./components/Oddy";
 
