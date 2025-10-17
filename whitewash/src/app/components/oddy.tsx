06ff3e07b90dfdd2049e4f66f330617a072447ca
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-10-17 21:16:29 +0200

    Fix froggy state

--- whitewash/src/app/components/oddy.tsx
+++ whitewash/src/app/components/oddy.tsx
@@ -1,19 +1,28 @@
+"use client"
+
 import Image from "next/image";
 import oddy from "../assets/images/oddy.png";
 import frog from "../assets/images/frog.png";
 import styles from "./Oddy.module.css";
+import { useEffect, useState } from "react";
 
 export const Oddy = ({
   message = "Hei! Jeg er Oddy, kan jeg hjelpe deg med noe?",
 }: {
   message?: string;
 }) => {
-  const isFrog = Math.random() < 1 / 20;
-  const avatar = isFrog ? frog : oddy;
-  const name = isFrog ? "Froggy" : "Oddy";
-  if (isFrog) {
-    message = message.replaceAll("Oddy", "Froggy");
-  }
+  const [isFrog, setIsFrog] = useState<boolean>(false)
+  const [avatar, setAvatar] = useState<typeof frog | typeof oddy>(oddy)
+  const [name, setName] = useState<"Froggy" | "Oddy">("Oddy")
+  
+  useEffect(() => {
+    const shouldBeFrog = Math.random() < 1 / 20
+    setIsFrog(shouldBeFrog)
+    if(shouldBeFrog) {
+      setAvatar(frog)
+      setName("Froggy")
+    }
+  }, [])
 
   return (
     <div className="flex items-end space-x-3">
@@ -25,7 +34,7 @@ export const Oddy = ({
         className={`rounded-full ${isFrog ? styles.spinSlow : ""}`}
       />
       <div className="relative bg-gray-200 px-4 py-3 rounded-lg text-gray-900 shadow">
-        {message}
+        {isFrog ? message.replaceAll("Oddy", "Froggy") : message}
         <div className="absolute -left-2 bottom-3 w-0 h-0 border-t-8 border-b-8 border-r-8 border-transparent border-r-gray-200" />
       </div>
     </div>
