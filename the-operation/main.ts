d76ed7813ea7876cc034c8037bd3e1459e04a927
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-10-17 20:39:07 +0200

    Create oddy api endpoint

--- the-operation/main.ts
+++ the-operation/main.ts
@@ -1,9 +1,17 @@
 import { neon } from "npm:@neondatabase/serverless"
 import { load } from "jsr:@std/dotenv"
+import { Ollama } from "npm:ollama"
 
 const env = await load({
   envPath: ".env",
 })
+
+const ollama = new Ollama({
+  host: "https://ollama.com",
+  headers: {
+    Authorization: "Bearer " + env.OLLAMA_API_KEY,
+  },
+});
 
 const sql = neon(env.NEON_CONNECTION_STRING)
 
@@ -18,6 +26,19 @@ Deno.serve({ port: 4000 }, async (req) => {
 		const products = await sql.query("SELECT * FROM products WHERE name ILIKE $1", [`%${search === null ? "" : search}%`])
 
 		return Response.json(products)
+	}
+
+	if(url.pathname == "/api/oddy") {
+		const params = url.searchParams
+
+		const oddyInput = params.get("inMessage")
+
+		const response = await ollama.chat({
+			model: "gpt-oss:120b",
+			messages: [{ role: "user", content: `You are Odd Reitan, CEO of Reitan Retail, one of the biggest retail companies in the nordics. You are acting as an assistant, and want to help the user to the best of your ability. The rest of this request will be in Norwegian. Please respond in Norwegian. Use plain text(no markdown) and respond breifly (ca. 150 characters max). The request from the user is: ${oddyInput}` }],
+		})
+
+		return Response.json(response.message.content)
 	}
 
   return new Response(url.pathname)
