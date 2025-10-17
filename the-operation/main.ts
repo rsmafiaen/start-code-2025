76ff20d0cd7661ffc8c6f709c1a28bb9fe695612
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-10-17 19:40:04 +0200

    Replace allProducts with findProducts

--- the-operation/main.ts
+++ the-operation/main.ts
@@ -1,5 +1,6 @@
 import { neon } from "npm:@neondatabase/serverless"
 import { load } from "jsr:@std/dotenv"
+import {  } from "npm:postgres"
 
 const env = await load({
   envPath: ".env",
@@ -10,8 +11,12 @@ const sql = neon(env.NEON_CONNECTION_STRING)
 Deno.serve({ port: 4000 }, async (req) => {
 	const url = new URL(req.url)
 
-	if(url.pathname == "/api/allProducts") {
-		const products = await sql`SELECT * FROM products`
+	if(url.pathname == "/api/findProducts") {
+		const params = url.searchParams
+
+		const search = params.get("search")
+
+		const products = await sql.query("SELECT * FROM products WHERE name ILIKE $1", [`%${search}%`])
 
 		return Response.json(products)
 	}
