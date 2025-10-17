5730c868b6e206cf39054f73542156086100a55c
Sivert Vårum <48734072+SivertVaarum@users.noreply.github.com>
2025-10-17 23:12:51 +0200

    Move components into src/app/components in whitewash (#21)
    Co-authored-by: Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>

--- whitewash/src/app/page.tsx
+++ whitewash/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import frawg from "../../frog.png";
-import { Header } from "../components/header";
-import { Oddy } from "./components/oddy";
+import { Header } from "./components/header";
+import { Oddy } from "./components/Oddy";
 
 export default function Home() {
   return (
