4dd7794da387a258820bf47f89acfaa181d19c82
HN05 <henrik.nordvik@gmail.com>
2025-10-17 20:08:06 +0200

    fixed feedback

--- whitewash/src/app/components/oddy.tsx
+++ whitewash/src/app/components/oddy.tsx
@@ -1,16 +1,15 @@
-import Image from 'next/image'
+import Image from "next/image";
+import oddy from "../assets/images/oddy.png";
 
-export default function Oddy({
+export const Oddy = ({
   message = "Hei! Jeg er Odd Reitan, kan jeg hjelpe deg med noe?",
 }: {
-  message?: string
-}) {
-  const imgPath = "/images/oddy.png"
-
+  message?: string;
+}) => {
   return (
     <div className="flex items-end space-x-3">
       <Image
-        src={imgPath}
+        src={oddy}
         alt="Oddy avatar"
         width={80}
         height={80}
@@ -18,8 +17,8 @@ export default function Oddy({
       />
       <div className="relative bg-gray-200 px-4 py-3 rounded-lg text-gray-900 shadow">
         {message}
-        <div className="absolute -left-2 bottom-3 w-0 h-0 border-t-8 border-b-8 border-r-8 border-transparent border-r-gray-200"></div>
+        <div className="absolute -left-2 bottom-3 w-0 h-0 border-t-8 border-b-8 border-r-8 border-transparent border-r-gray-200" />
       </div>
     </div>
-  )
-}
+  );
+};
--- whitewash/src/app/page.tsx
+++ whitewash/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import Oddy from "./components/oddy";
+import { Oddy } from "./components/oddy";
+import frog from "./assets/images/frog.png";
 
 export default function Home() {
   return (
     <div>
-      <Image src="/images/frog.png" width={200} height={200} alt="frog." />
-      <Oddy/>
+      <Image src={frog} width={200} height={200} alt="frog." />
+      <Oddy />
     </div>
   );
 }
