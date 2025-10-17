31505ec0e2cd84caf73b5ae68dcaad3e731a5f4c
Nicolay B. Rennemo <rennemo@proton.me>
2025-10-18 00:33:06 +0200

    Restructure and lint whitewash

--- whitewash/src/app/components/oddy/Oddy.tsx
+++ whitewash/src/app/components/oddy/Oddy.tsx
@@ -1,31 +1,31 @@
 "use client"
 
-import Image from "next/image";
-import oddy from "../assets/images/oddy.png";
-import frog from "../assets/images/frog.png";
-import styles from "./Oddy.module.css";
-import { useEffect, useState } from "react";
+import Image from "next/image"
+import oddy from "@/assets/images/oddy.png"
+import frog from "@/assets/images/frog.png"
+import styles from "./Oddy.module.css"
+import { useEffect, useState } from "react"
 
 export const Oddy = ({
   message = "Hei! Jeg er Oddy, kan jeg hjelpe deg med noe?",
 }: {
-  message?: string;
+  message?: string
 }) => {
   const [isFrog, setIsFrog] = useState<boolean>(false)
   const [avatar, setAvatar] = useState<typeof frog | typeof oddy>(oddy)
   const [name, setName] = useState<"Froggy" | "Oddy">("Oddy")
-  
+
   useEffect(() => {
     const shouldBeFrog = Math.random() < 1 / 20
     setIsFrog(shouldBeFrog)
-    if(shouldBeFrog) {
+    if (shouldBeFrog) {
       setAvatar(frog)
       setName("Froggy")
     }
   }, [])
 
   return (
-    <div className="flex items-end space-x-3">
+    <div className="flex items-end bottom-0 space-x-3">
       <Image
         src={avatar}
         alt={`${name} avatar`}
@@ -38,5 +38,5 @@ export const Oddy = ({
         <div className="absolute -left-2 bottom-3 w-0 h-0 border-t-8 border-b-8 border-r-8 border-transparent border-r-gray-200" />
       </div>
     </div>
-  );
-};
+  )
+}
