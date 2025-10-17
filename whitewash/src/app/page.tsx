7e88a1cad93d9242808388356c081de7b5d90139
Kristian <k.numme05@gmail.com>
2025-10-17 21:21:40 +0200

    header added to home page

--- whitewash/src/app/page.tsx
+++ whitewash/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import frawg from "../../frog.png";
+import { Header } from "../components/header";
 
 export default function Home() {
   return (
     <div>
+      <Header></Header>
       <Image src={frawg} alt="frog." />
     </div>
   );
