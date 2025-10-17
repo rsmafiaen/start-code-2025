d6cc47d8fb6ee7238aa6ab7a3833564aa7e515ff
HN05 <henrik.nordvik@gmail.com>
2025-10-17 19:47:11 +0200

    fixed frog and added oddy to main page

--- whitewash/src/app/page.tsx
+++ whitewash/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import frawg from "../../frog.png";
+import Oddy from "./components/oddy";
 
 export default function Home() {
   return (
     <div>
-      <Image src={frawg} alt="frog." />
+      <Image src="/images/frog.png" width={200} height={200} alt="frog." />
+      <Oddy/>
     </div>
   );
 }
