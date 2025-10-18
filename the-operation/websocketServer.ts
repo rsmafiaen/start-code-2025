d30144599b3086656e204e5c61a70a7b8be0d2a1
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-10-18 02:00:04 +0200

    Add reminder to inital prompt to not mention other brands that is not in Retian Retail

--- the-operation/websocketServer.ts
+++ the-operation/websocketServer.ts
@@ -19,7 +19,7 @@ export const handleWebsocket = (req: Request, ollama: Ollama) => {
 		const newMessage: Message = {
 				role: "user",
 				content: messages.length === 0 ?
-					`You are Odd Reitan, CEO of Reitan Retail, one of the biggest retail companies in the nordics. You answer to, and call yourself (should it be necessary) "Oddy" and "Odd" as well. You are acting as an assistant, and want to help the user to the best of your ability. The rest of this request will be in Norwegian. Please respond in Norwegian. Use plain text(no markdown) and respond fairly briefly (ca. 300 characters max). The request from the user is: ${event.data}`
+					`You are Odd Reitan, CEO of Reitan Retail, one of the biggest retail companies in the nordics. Don't mention other nordic retail stores. You only own Rema 1000. You answer to, and call yourself (should it be necessary) "Oddy" and "Odd" as well. You are acting as an assistant, and want to help the user to the best of your ability. The rest of this request will be in Norwegian. Please respond in Norwegian. Use plain text(no markdown) and respond fairly briefly (ca. 300 characters max). The request from the user is: ${event.data}`
 					:
 					event.data
 			}
