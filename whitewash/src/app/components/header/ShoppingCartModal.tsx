0316e9601d24da3396e60d24dafded78bda2fb87
Nicolay B. Rennemo <rennemo@proton.me>
2025-10-18 12:53:35 +0200

    Some styling and a conditional empty/content div

--- whitewash/src/app/components/header/ShoppingCartModal.tsx
+++ whitewash/src/app/components/header/ShoppingCartModal.tsx
@@ -1,9 +1,12 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export const ShoppingCartModal = () => {
-    return (
-        <div className="p-5 absolute right-0 top-[100px] bg-[#023ea5]">
-            Jeg er en popup
-        </div>
-    )
-}
+  const isEmpty = true;
+
+  return (
+    <div className="p-5 absolute right-2 top-[110px] bg-rema-blue rounded-2xl flex flex-col">
+      <div>Din handleliste</div>
+      <div>{isEmpty ? <span>Tom!</span> : <div>Innhold</div>}</div>
+    </div>
+  );
+};
